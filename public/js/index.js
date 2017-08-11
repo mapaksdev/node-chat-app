75cb057bff4cda5b6d823d2a90180bbe8f8eb902
Mapaks <nlumapac@gmail.com>
2017-08-11 10:01:19 +0800

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    from: 'lermie@lermie.com',
-    text: 'Hi dady!'
-  });
 });
 
 socket.on('disconnect', function () {
